caf0290517e99a5b52af277f1186f8eabf312768
leondal <66068219+leondal@users.noreply.github.com>
2021-03-16 08:35:20 -0400

    Update _spike_growth.c

--- cmds/spells/s/_spike_growth.c
+++ cmds/spells/s/_spike_growth.c
@@ -16,7 +16,6 @@ void create()
     ::create();
     set_spell_name("spike growth");
     set_spell_level(([ "druid" : 3, "ranger" : 2 ]));
-    set_domains("earth");
     set_spell_sphere("conjuration_summoning");
     set_syntax("cast CLASS spike growth");
     set_description("This spell causes roots or other vegetation in the area to become hard and razor sharp.  Anyone moving in the area, "
