2da6d41f20ef0138f9c2668a9a18228ecd80abbc
Yves <mana.starved@yahoo.com>
2021-03-18 11:20:43 -0700

    Put recognize all back for imms, fix help color codes

--- cmds/mortal/_recognize.c
+++ cmds/mortal/_recognize.c
@@ -17,6 +17,18 @@ int cmd_recognize(string str)
     if (!str) {
         return help();
     }
+    if(str == "all"){
+       if(!avatarp(TP) && !TP->query("test_character")){
+          tell_object(TP,"Sorry, only immortals can use this command.");
+          return 1;
+       }
+       usrs = users();
+       for(x = 0;x < sizeof(usrs);x++) {
+           TP->addRelationship(usrs[x],usrs[x]->query_name());
+       }
+       write("All players online have been recognized.");
+       return 1;
+   }
     if (sscanf(str, "%s as %s", who, as) != 2) {
         return help();
     }
@@ -60,23 +72,23 @@ int cmd_recognize(string str)
 int help()
 {
     write(
-"%^CYAN%^NAME%^RESET % ^
+"%^CYAN%^NAME%^RESET%^
 
 recognize - recognize someone
 
-%^CYAN%^SYNOPSIS%^RESET % ^
+%^CYAN%^SYNOPSIS%^RESET%^
 
-recognize%^ORANGE%%^ULINE%^WHO%^RESET%^as%^ORANGE%%^ULINE%^NAME%^RESET % ^
+recognize %^ORANGE%^%^ULINE%^WHO%^RESET%^ as %^ORANGE%^%^ULINE%^NAME%^RESET%^
 
-%^CYAN%^DESCRIPTION%^RESET % ^
+%^CYAN%^DESCRIPTION%^RESET%^
 
-Allows you to recognize another player by an id, such as race, and assign them a%^ORANGE%%^ULINE%^NAME%^RESET % ^.
+Allows you to recognize another player by an id, such as race, and assign them a %^ORANGE%^%^ULINE%^NAME%^RESET%^.
 
 Recognizing someone for the first time grants you a small amount of exp towards next level.
 
 You can recall list of recognized people with <recall relationships>.
 
-%^CYAN%^SEE ALSO%^RESET % ^
+%^CYAN%^SEE ALSO%^RESET% ^
 
 recall, who, mail
 "
