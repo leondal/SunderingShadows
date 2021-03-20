1ba501b585df3fb04ce9bbd6bb72a272529f4426
leondal <66068219+leondal@users.noreply.github.com>
2021-03-20 11:10:57 -0400

    Update _weird.c

--- cmds/spells/w/_weird.c
+++ cmds/spells/w/_weird.c
@@ -10,7 +10,8 @@ void create()
 {
     ::create();
     set_spell_name("weird");
-    set_spell_level(([ "mage" : 9, "oracle" : 9 ]));
+    set_spell_level(([ "mage" : 9, "oracle" : 9, "cleric" : 9 ]));
+    set_domains("madness");
     set_mystery("whimsy");
     set_spell_sphere("illusion");
     set_syntax("cast CLASS weird");
