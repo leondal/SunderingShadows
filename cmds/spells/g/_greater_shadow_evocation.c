eb4f834391f9ba0a14adf8d5c161ab9ed9221f70
leondal <66068219+leondal@users.noreply.github.com>
2021-03-19 23:16:56 -0400

    Update _greater_shadow_evocation.c

--- cmds/spells/g/_greater_shadow_evocation.c
+++ cmds/spells/g/_greater_shadow_evocation.c
@@ -6,7 +6,6 @@ void create()
     set_spell_name("greater shadow evocation");
     set_spell_level(([ "mage" : 8, "oracle" : 8 ]));
     set_mystery(({"darkness","shadow"}));
-    set_domains("darkness");
     set_spell_sphere("illusion");
     set_syntax("cast CLASS greater shadow evocation on SPELL_NAME [on SPELL_ARGS]");
     set_description("You tap energy from the Plane of Shadow to cast a quasi-real, illusory version of an evocation mage spell of 7th level or lower.
