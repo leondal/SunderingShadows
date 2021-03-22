860459fcc43fa6d2188f3c41585a61674b1b8e95
leondal <66068219+leondal@users.noreply.github.com>
2021-03-22 15:48:39 -0700

    Update _orb_of_the_void.c

--- cmds/spells/o/_orb_of_the_void.c
+++ cmds/spells/o/_orb_of_the_void.c
@@ -11,7 +11,7 @@ void create()
 {
     ::create();
     set_spell_name("orb of the void");
-    set_spell_level(([ "mage" : 8,]));
+    set_spell_level(([ "mage" : 8, "cleric" : 8 ]));
     set_spell_sphere("necromancy");
     set_syntax("cast CLASS orb of the void");
     set_damage_desc("mass negative levels on living");
