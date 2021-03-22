dad8f1ae0f272e4a7aa320f5d23e19460f21c506
shadowgate <law@shadowgate.org>
2021-03-22 22:30:05 +0000

    Autocommit

--- std/user.c
+++ std/user.c
@@ -1361,6 +1361,7 @@ void setup()
         sight_bonus = (int)RACE_D->query_sight_bonus(query("race"));
 
         if(member_array("cavern", TO->query_divine_domain()) >= 0 ||
+           member_array("moon", this_object()->query_divine_domain()) >= 0 ||
            member_array("darkness", TO->query_divine_domain()) >= 0)
             sight_bonus += 2;
 
