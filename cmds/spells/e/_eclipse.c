9167231f08594351bc057f063743e8fb9fd241f8
leondal <66068219+leondal@users.noreply.github.com>
2021-03-22 15:04:59 -0700

    Update _eclipse.c

--- cmds/spells/e/_eclipse.c
+++ cmds/spells/e/_eclipse.c
@@ -11,7 +11,8 @@ void create()
 {
     ::create();
     set_spell_name("eclipse");
-    set_spell_level(([ "mage" : 9, "oracle" : 8 ]));
+    set_spell_level(([ "mage" : 9, "oracle" : 8, "cleric" : 9 ]));
+    set_domains("moon");
     set_mystery("lunar");
     set_spell_sphere("conjuration_summoning");
     set_syntax("cast CLASS eclipse");
