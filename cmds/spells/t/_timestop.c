c04ce609559ba546b0f87c6d32c22f0ab1280ce0
leondal <66068219+leondal@users.noreply.github.com>
2021-03-23 15:38:57 -0400

    Update _timestop.c

--- cmds/spells/t/_timestop.c
+++ cmds/spells/t/_timestop.c
@@ -8,7 +8,6 @@ void create() {
     set_author("nienne");
     set_spell_name("timestop");
     set_spell_level(([ "mage" : 9, "bard" : 6, "oracle":9, "psion" : 9 ]));
-    set_domains("trickery");
     set_discipline("nomad");
     set_mystery("lore");
     set_spell_sphere("alteration");
