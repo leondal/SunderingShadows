b3278a8719f6bd69bc2324f0fedcc7a03ec9b8ac
leondal <66068219+leondal@users.noreply.github.com>
2021-03-23 15:40:34 -0400

    Update _screen.c

--- cmds/spells/s/_screen.c
+++ cmds/spells/s/_screen.c
@@ -15,9 +15,8 @@ object detector, blocker;
 void create() {
     ::create();
     set_spell_name("screen");
-    set_spell_level(([ "oracle" : 8, "mage": 8, "cleric" : 8 ]));
+    set_spell_level(([ "oracle" : 8, "mage": 8 ]));
     set_spell_sphere("illusion");
-    set_domains("trickery");
     set_mystery("lore");
     set_syntax("cast CLASS screen");
     set_description("Upon casting this spell, the user creates a screen of illusion centered upon himself that makes "
