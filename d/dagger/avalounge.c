e2c57219ed62003518906dd32c93fb7a4013eee9
shadowgate <law@shadowgate.org>
2021-03-28 13:30:06 +0000

    Autocommit

--- d/dagger/avalounge.c
+++ d/dagger/avalounge.c
@@ -32,7 +32,7 @@ void create()
     ::create();
     set_property("light", 2);
     set_property("indoors", 1);
-   set_property("no scry",1); // added by Lujke 12 November 2007
+    set_property("no scry",1); // added by Lujke 12 November 2007
     set_property("no teleport",1);
     set_short("Avatar's Lounge");
     set_long(
@@ -57,33 +57,34 @@ OLI
     set_smell("default", "You smell the fresh scent of summer flowers.");
     set_listen("default", "You hear a light hum in the background.");
     set_exits( ([
-      "shadow" : "/d/shadow/city/room/plaza", //Updated to new Shadow by Titania
-      "scourge" : "/d/dagger/tonovi/1path1.c", 
-      "rhap" : "/d/darkwood/room/road7.c", 
-      "tonovi" : "/d/dagger/tonovi/town/street5.c", 
-      "dagger" : "/d/dagger/nurval/room/village", 
-      "deku" : "/d/deku/town/road5.c", 
-      "down" : "/d/shadow/city/room/plaza",
-      "newbietown" :"/d/newbie/rooms/town/square",
-      "Sjail":"/d/shadow/room/city/jail_main",
-      //"arena" : "/realms/obsidian/rooms/arena1",
-//     "fenceroom" : "/realms/garrett/fenceroom",
-      "east" : "/d/dagger/avaboard2",
-      "north"  :  "/d/dagger/stdsboard",
-      "southwest" : "/d/dagger/spawn_room",
-      "northwest" : "/d/dagger/wiz_store2",
-      "tabor" : "/d/darkwood/tabor/room/cnchall",
-      "south" : "/d/dagger/planroom",
-      "portals" :"/d/dagger/portals",
-      "lib":"/d/dagger/lib_room",
-      "juran" :"/d/shadow/juran/city/quest_room", //Added by saide
-      "boards":"/d/dagger/boards_portal",
-      "antioch" : "/d/antioch/antioch2/rooms/street2", //Added by saide
-      "seneca" : "/d/attaya/newseneca/rooms/inn_entrance", //fixed by ~Circe~ to go to the board
-      "torm" : "/d/dagger/Torm/city/c45", //added by saide
-      "garretthome": "/realms/garrett/workroom", // added by Garrett ot give workroom access.
-      "VOID": "/d/shadowgate/void.c",
-      "FREEZER": "/d/shadowgate/freezer.c",
+        "shadow" : "/d/shadow/city/room/plaza", //Updated to new Shadow by Titania
+        "scourge" : "/d/dagger/tonovi/1path1.c", 
+        "rhap" : "/d/darkwood/room/road7.c", 
+        "tonovi" : "/d/dagger/tonovi/town/street5.c", 
+        "dagger" : "/d/dagger/nurval/room/village", 
+        "deku" : "/d/deku/town/road5.c", 
+        "down" : "/d/shadow/city/room/plaza",
+        "newbietown" :"/d/newbie/rooms/town/square",
+        "Sjail":"/d/shadow/room/city/jail_main",
+//      "arena" : "/realms/obsidian/rooms/arena1",
+//      "fenceroom" : "/realms/garrett/fenceroom",
+        "east" : "/d/dagger/avaboard2",
+        "north"  :  "/d/dagger/stdsboard",
+        "southwest" : "/d/dagger/spawn_room",
+        "northwest" : "/d/dagger/wiz_store2",
+        "tabor" : "/d/darkwood/tabor/room/cnchall",
+        "south" : "/d/dagger/planroom",
+        "portals" :"/d/dagger/portals",
+        "lib":"/d/dagger/lib_room",
+        "juran" :"/d/shadow/juran/city/quest_room", //Added by saide
+        "boards":"/d/dagger/boards_portal",
+        "antioch" : "/d/antioch/antioch2/rooms/street2", //Added by saide
+        "seneca" : "/d/attaya/newseneca/rooms/inn_entrance", //fixed by ~Circe~ to go to the board
+        "torm" : "/d/dagger/Torm/city/c45", //added by saide
+        "garretthome": "/realms/garrett/workroom", // added by Garrett ot give workroom access.
+        "VOID": "/d/shadowgate/void.c",
+        "FREEZER": "/d/shadowgate/freezer.c",
+        "dummy" : "/d/dagger/dummyroom.c", // added by Chernobog
     ]) );
 }
 
@@ -105,58 +106,59 @@ void init(){
 }
 
 int push(string str) {
-   if(!str) {
-      write("You might want to <push button #>.");
-      return 1;
-   }
-   if(str == "button 1") { 
-      new("/d/common/immortals/wand")->move(TP);
-      write("You receive a strange stick.");
-      return 1;
-   }
-   if(str == "button 2") {
-      new("/d/common/obj/special/ambassador_ring")->move(TP);
-      write("You receive a cool brass ring and might want to <review commands>.");
-      return 1;
-   }
-   if(str == "button 3") { 
-      new("/d/common/immortals/prof_clearer.c")->move(TP);
-      write("You receive a strange device used to reset profs.");
-      return 1;
-   }
-   if (str == "button 4"){
-      new("/d/common/immortals/clone_wand")->move(TP);
-      write("You receive a wand that seems a lot like a lot of other wands.");
-      return 1;
-  }
+    if(!str) {
+        write("You might want to <push button #>.");
+        return 1;
+    }
+    if(str == "button 1") { 
+        new("/d/common/immortals/wand")->move(TP);
+        write("You receive a strange stick.");
+        return 1;
+    }
+    if(str == "button 2") {
+        new("/d/common/obj/special/ambassador_ring")->move(TP);
+        write("You receive a cool brass ring and might want to <review commands>.");
+        return 1;
+    }
+    if(str == "button 3") { 
+        new("/d/common/immortals/prof_clearer.c")->move(TP);
+        write("You receive a strange device used to reset profs.");
+        return 1;
+    }
+    if (str == "button 4"){
+        new("/d/common/immortals/clone_wand")->move(TP);
+        write("You receive a wand that seems a lot like a lot of other wands.");
+        return 1;
+    }
    if (str == "button 5"){
       new("/d/common/immortals/undercommon_wand")->move(TP);
       write("You receive a wand for granting undercommon to players.");
       return 1;
    }
-   if (str =="button 6"){
-      new("/d/common/immortals/id_adder")->move(TP);
-      write("You recieve a nifty id adder, for adding new ids to mobs. "
-           +" You can <read adder> for instructions.");
-     return 1;
-   }
-   return 0;
+    if (str =="button 6"){
+        new("/d/common/immortals/id_adder")->move(TP);
+        write("You receive a nifty id adder, for adding new ids to mobs. "
+            +" You can <read adder> for instructions.");
+        return 1;
+    }
+    return 0;
 }
 
 int pull(string str) {
-   if(!str) {
-      write("You might want to <pull lever/cord>.");
-      return 1;
-   }
-   if(str == "lever") { 
+    if(!str) {
+        write("You might want to <pull lever/cord>.");
+        return 1;
+    }
+    if(str == "lever") { 
       new("/realms/crystal/obj/cube")->move(TP);
-      write("You recieve a strange and magical cube.");
+      write("You receive a strange and magical cube.");
       return 1;
    }
    if(str == "cord") {
-      new("/d/common/immortals/universalremote")->move(TP);
-      write("You receive the AWESOME new universal remote!");
-      return 1;
-   }
-   return 0;
+        new("/d/common/immortals/universalremote")->move(TP);
+        write("You receive the AWESOME new universal remote!");
+        return 1;
+    }
+    return 0;
 }
+
