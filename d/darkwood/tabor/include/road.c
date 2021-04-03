370d8fc77d5f13cb77d88af4a58601807a57b84a
Ti-SuS <80554451+Ti-SuS@users.noreply.github.com>
2021-04-02 20:02:18 -0400

    Update road.c
    Prettying.

--- d/darkwood/tabor/include/road.c
+++ d/darkwood/tabor/include/road.c
@@ -2,22 +2,20 @@
 inherit ROOM;
 
 string * features = ({
-        "%^GREEN%^A small %^BLUE%^creek%^GREEN%^ meanders through the forest.%^RESET%^",
-            "%^BOLD%^%^BLACK%^The ancient bricks here have severely eroded, and only a few can be seen.%^RESET%^",
-            "%^GREEN%^A %^ORANGE%^wooden bridge%^GREEN%^ crosses a small %^BLUE%^river%^GREEN%^ that flows from the forest.%^RESET%^",
-            "%^WHITE%^A mountain range with %^BOLD%^snowy peaks%^RESET%^ towers over the trees in the south.",
-            "%^GREEN%^Dense forest surrounds you on all sides.",
-            "%^GREEN%^The road bears distinctive dirty %^ORANGE%^goblin footprints%^RESET%^.",
-            "%^GREEN%^You hear a beautiful melody coming from the forest.",
-            "%^GREEN%^Ancient ruins of a stone %^BOLD%^%^BLACK%^watchtower%^RESET%^%^GREEN%^ stand off to the side.%^RESET%^",
-            "%^GREEN%^The road passes through a %^MAGENTA%^flowery meadow%^GREEN%^.",
-            "%^GREEN%^A %^BOLD%^%^BLACK%^gravestone%^RESET%^%^GREEN%^ stands on the side of the road.%^RESET%^",
-            "%^GREEN%^An abandoned %^ORANGE%^wagon cart%^GREEN%^ is on the side of the road. You see some %^BOLD%^%^WHITE%^human bones%^RESET%^%^GREEN%^ by it.%^RESET%^",
-            "%^GREEN%^You see %^RED%^a pool of blood%^GREEN%^ on the ground.%^RESET%^",
-            "%^GREEN%^A disfigured fresh human corpse lies on the forest side with its head bitten off. %^RED%^Blood%^GREEN%^ covers the grass.%^RESET%^",
-            "%^GREEN%^The %^BOLD%^canopy%^RESET%^%^GREEN%^ is especially dense here.%^RESET%^",
-            "%^GREEN%^Some riders passed through here recently.%^RESET%^",
-            "%^GREEN%^The surrounding %^BOLD%^%^BLACK%^tall trees%^RESET%^%^GREEN%^ are very old.%^RESET%^",
+            "%^GREEN%^A small creek meanders through the forest, gently burbling away.%^RESET%^",
+            "%^GREEN%^The dirt beneath your feet has eroded enough to reveal some crumbling ancient bricks.%^RESET%^",
+            "%^GREEN%^A wooden bridge rises over a small river that flows from the forest.%^RESET%^",
+            "%^GREEN%^A mountain range, its snowy peaks stretching toward the clouds, towers over the trees in the south.%^RESET%^",
+            "%^GREEN%^The dense trees form a ceiling overhead, blotting out the light.%^RESET%^",
+            "%^GREEN%^Dozens of goblins footprints scramble all over the road, they must live nearby.%^RESET%^",
+            "%^GREEN%^There is an enchanting melody coming from the forest, calling to you.%^RESET%^",
+            "%^GREEN%^You pass by the crumbling ruins of an ancient stone watchtower.%^RESET%^",
+            "%^GREEN%^The road curves through a wide meadow.%^RESET%^",
+            "%^GREEN%^A lone gravestone is tucked back in some foliage alongside the road.%^RESET%^",
+            "%^GREEN%^Some fresh blood is smeared across the road here, but there are no signs of where it came from.%^RESET%^",
+            "%^GREEN%^The foliage is quite dense here, making it difficult to see through the trees.%^RESET%^",
+            "%^GREEN%^Some riders passed through here recently, their horses leaving behind heavy hoof marks.%^RESET%^",
+            "%^GREEN%^The trees tower ever upwards, ancient and awe-inspiring.%^RESET%^",
             });
 
 
@@ -30,8 +28,9 @@ void create()
    set_short("Road between Azha and Tabor");
    set_long(
    "%^BOLD%^%^GREEN%^On the Northern Highway, connecting Azha and Tabor%^RESET%^\n"+
-   "%^GREEN%^The road is large enough for two wagons to travel side by side and it has obviously had a lot of use, the dirt is hard and compacted and has baked in the often blistering heat of the sun. It has apparently fallen into a bit of disrepair over the last several years, several of the stones that border the road are missing or completely covered in mud."+make_fdesc()+"
-"
+   "%^GREEN%^This road is large enough for two wagons to travel side by side, and it has obviously seen a lot of use. The dirt is hard and compacted, and has baked in the often blistering heat of the sun. It has apparently fallen into a bit of disrepair over the last several years. Several of the stones that border the road are missing or completely covered in mud."+make_fdesc()+"%^WHITE%^
+
+%^WHITE%^"
    );
    set_listen("default","The road is fairly quiet with few traveling along it.");
    set_smell("default","The dust you've kicked up on the road hangs in the air.");
