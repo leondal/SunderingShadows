70c4fc71693343241b3dfb03fbaf00c2f3bb8f4f
SuS-Tsera <80545998+SuS-Tsera@users.noreply.github.com>
2021-04-10 19:31:03 -0700

    Update _reward.c

--- cmds/mortal/_reward.c
+++ cmds/mortal/_reward.c
@@ -31,11 +31,11 @@ int cmd_reward(string str)
         target->set_property("ignore tax", 1);
         target->add_general_exp(target->query_classes()[0], expall);
         target->remove_property("ignore tax");
-        tell_object(TP, "%^CYAN%^%^BOLD%^You have rewarded everybody present.");
         tell_object(target, "%^CYAN%^%^BOLD%^You feel enlightened as your powers grow.");
         TP->delete("last_reward");
         TP->set("last_reward", time());
 	   }
+	   tell_object(TP, "%^CYAN%^%^BOLD%^You have rewarded everybody present.");
 	   return 1;
 	}
 	
