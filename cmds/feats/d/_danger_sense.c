8671ed45a699ccf6a404c2b0b1ef8e09e88f1e26
shadowgate <law@shadowgate.org>
2021-04-17 13:30:05 +0000

    Autocommit

--- cmds/feats/d/_danger_sense.c
+++ cmds/feats/d/_danger_sense.c
@@ -9,7 +9,7 @@ void create() {
     feat_category("Evasion");
     feat_name("danger sense");
     feat_prereq("Barbarian L5 or Thief L5");
-    feat_desc("This feat gives the barbarian or thief an uncanny sense of danger around them. They gain a scaling bonus to reflex saves and perception skill (max 6). They also become harder for thieves to catch them unaware, reducing sneak attack dice by half against them.");
+    feat_desc("This feat gives the barbarian or thief an uncanny sense of danger around them. They gain a scaling bonus to reflex saves and perception skill (max 6). They also become harder for thieves to catch them unaware, reducing sneak attack dice by half against them (as long as the enemy is no more than 4 levels higher than them).");
 }
 
 int allow_shifted() { return 1; }
