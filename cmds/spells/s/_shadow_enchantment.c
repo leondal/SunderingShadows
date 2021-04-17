044c45168b934024ee0b4c79b5c73b304484e8e3
shadowgate <law@shadowgate.org>
2021-04-17 03:30:03 +0000

    Autocommit

---
+++ cmds/spells/s/_shadow_enchantment.c
@@ -0,0 +1,19 @@
+inherit "/cmds/spells/s/_shadow_evocation";
+
+void create()
+{
+    ::create();
+    set_spell_name("shadow enchantment");
+    set_spell_level(([ "mage" : 5, "bard" : 5 ]));
+    set_spell_sphere("illusion");
+    set_syntax("cast CLASS shadow enchantment on SPELL_NAME [on SPELL_ARGS]");
+    set_description("You tap energy from the Plane of Shadow to cast a quasi-real, illusory version of an enchantment mage spell of 4th level or lower.
+
+%^BOLD%^%^RED%^E.G.%^RESET%^ <cast mage shadow alteration on haste on friend>");
+    set_arg_needed(1);
+}
+
+string shadow_school()
+{
+    return "enchantment_charm";
+}
