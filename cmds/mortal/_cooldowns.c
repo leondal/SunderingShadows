45385a5d303e2b80f6491a6ef35cf4fb3b3c5ace
shadowgate <law@shadowgate.org>
2021-04-18 02:30:05 +0000

    Autocommit

---
+++ cmds/mortal/_cooldowns.c
@@ -0,0 +1,44 @@
+/*
+  cooldowns.c
+  
+  Shows your current cooldowns and time left.
+  
+  -- Tlaloc --
+*/
+
+#include <std.h>
+#include <daemons.h>
+
+#define LINE sprintf("%s[ Cooldowns ]%s\n", "*=*=*=*=*=*", "*=*=*=*=*=*")
+
+inherit DAEMON;
+
+mixed cmd_cooldowns(string args)
+{
+    object who = this_player();
+    mapping cooldowns;
+    string *keys;
+    int width;
+    
+    cooldowns = who->query_cooldowns();
+    keys = keys(cooldowns);
+    
+    write("\n");
+    write(LINE);
+    
+    if(sizeof(keys))
+    {
+        foreach(string str in keys)
+        {
+            tell_object(who, sprintf("   %-13s : %12i\n",capitalize(str), (cooldowns[str] - time())));
+        }
+    }
+    else
+        write("               %^BOLD%^None%^RESET%^\n");
+    
+    write(LINE);
+    return 1;
+}
+    
+    
+    
