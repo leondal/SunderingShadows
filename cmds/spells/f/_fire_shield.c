3b136db69850d834e54be4246c74661988dbea85
shadowgate <law@shadowgate.org>
2021-04-19 13:00:06 +0000

    Autocommit

--- cmds/spells/f/_fire_shield.c
+++ cmds/spells/f/_fire_shield.c
@@ -109,7 +109,7 @@ void execute_attack(){
         for(i=0;i<sizeof(foes);i++){
             tell_object(foes[i],"%^BOLD%^"+scolor+"You are burned by the shield of flames as you strike "
                         ""+caster->QCN+"!");
-            if(do_save(foes[i], 0))
+            if(foes[i] && do_save(foes[i], 0))
             {
                 damage_targ(foes[i],foes[i]->return_target_limb(),sdamage / 2,element);
             } else {
