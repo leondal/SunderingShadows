e9953d5505024ebf160947117f0f04ca527ebdee
Ti-SuS <80554451+Ti-SuS@users.noreply.github.com>
2021-04-18 20:08:07 -0400

    Update _shadow_enchantment.c

--- cmds/spells/s/_shadow_enchantment.c
+++ cmds/spells/s/_shadow_enchantment.c
@@ -7,7 +7,7 @@ void create()
     set_spell_level(([ "mage" : 5, "bard" : 5 ]));
     set_spell_sphere("illusion");
     set_syntax("cast CLASS shadow enchantment on SPELL_NAME [on SPELL_ARGS]");
-    set_description("You tap energy from the Plane of Shadow to cast a quasi-real, illusory version of an enchantment mage spell of 4th level or lower.
+    set_description("You create an illusory version of an enchantment spell of 4th level or lower. The source of the illusion can differ based on the caster.
 
 %^BOLD%^%^RED%^E.G.%^RESET%^ <cast mage shadow enchantment on haste on friend>");
     set_arg_needed(1);
