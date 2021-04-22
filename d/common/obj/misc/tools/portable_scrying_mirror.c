3c8637a78e8b85f13d1570fe905eaf4719648b5e
Yves <mana.starved@yahoo.com>
2021-04-22 13:48:20 -0700

    Revert changes that stopped portable scrying devices from working properly

--- d/common/obj/misc/tools/portable_scrying_mirror.c
+++ d/common/obj/misc/tools/portable_scrying_mirror.c
@@ -11,7 +11,8 @@ create() {
 	set_short("%^RESET%^%^CYAN%^a portable scrying mirror%^RESET%^");
 	set_property("no animate", 1);
 	set_long("This is a rather plain, but finely made mirror.  The reflective surface appears to be silver polished to an incredible lustre.  The surface is large and with the stout frame, this mirror looks to weigh quite a bit, and is even bulkier than it is heavy.");
-	set_weight(15);
+	set_value(75000);
+	set_weight(20);
 	set_no_clean(1);
 }
 
