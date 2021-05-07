d5c729d3f685131df6e79b4b41b56668c88314ab
shadowgate <law@shadowgate.org>
2021-05-07 16:00:06 +0000

    Autocommit

--- daemon/combat_d.c
+++ daemon/combat_d.c
@@ -567,7 +567,7 @@ void check_extra_abilities(object attacker, object target, object weapon, int cr
             target->is_undead() &&
             FEATS_D->usable_feat(attacker, "smite the lifeless")) {
             //Needs adjusting to not one-shot Vecna/intruder but also be effective against smaller bosses
-            if (target->query_hp_percent() < 80) {
+            if (target->query_hp_percent() < 90) {
                 if (!target->fort_save(max( ({ BONUS_D->query_stat_bonus(attacker, "strength"), BONUS_D->query_stat_bonus(attacker, "dexterity") }) )) && (userp(target) ? target->query_level() <= attacker->query_level() : target->query_level() < 56)) {
                     tell_object(attacker, "%^BOLD%^You unleash a flash of blinding white energy as you destroy " + target->QCN + "'s undead energy and end them!%^RESET%^");
                     tell_object(target, "%^BOLD%^" + attacker->QCN + " unleashes a flash of blinding white light that ends your undead existence!%^RESET%^");
