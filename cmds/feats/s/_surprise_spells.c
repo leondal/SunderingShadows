04e494c591c735bd76e8ea8930d6910a35f5abc2
shadowgate <law@shadowgate.org>
2021-05-14 19:30:05 +0000

    Autocommit

--- cmds/feats/s/_surprise_spells.c
+++ cmds/feats/s/_surprise_spells.c
@@ -10,7 +10,7 @@ void create()
     feat_category("ArcaneTrickster");
     feat_name("surprise spells");
     feat_prereq("Arcane Trickster L7");
-    feat_desc("An arcane trickster is well versed at using spells and stealth in concert. Their Spellcraft and Stealth skills become focused and augmented. All of their spells will be silenced. Finally, while invisible or hidden, the Arcane Trickster gets a +3 to spell level against targets in the same room with them.");
+    feat_desc("An arcane trickster is well versed at using spells and stealth in concert. Their Spellcraft and Stealth skills become focused and augmented. All of their spells will be silenced. Finally, while invisible or hidden, the Arcane Trickster gets a +3 to spell DC against targets in the same room with them.");
     permanent(1);
 }
 
