9a88018a12e8a3c3e5baf9b3627a31f5c1c171a3
leondal <66068219+leondal@users.noreply.github.com>
2021-05-15 16:59:24 -0700

    Update _animal_affinity.c

--- cmds/spells/a/_animal_affinity.c
+++ cmds/spells/a/_animal_affinity.c
@@ -11,7 +11,7 @@ void create() {
     ::create();
     set_spell_name("animal affinity");
     set_spell_level(([ "psywarrior" : 2, "psion" : 1 ]));
-    set_spell_sphere("alteration");
+    set_spell_sphere("psychometabolism");
     set_discipline("egoist");
     set_bonus_type("enhancement");
     set_syntax("cast CLASS animal affinity on <stat>");
