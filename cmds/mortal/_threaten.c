31334349956532c27fafefa762af0d1bb96c6366
Yves <mana.starved@yahoo.com>
2021-05-25 16:15:26 -0700

    Update threaten to account for the various PK flags

--- cmds/mortal/_threaten.c
+++ cmds/mortal/_threaten.c
@@ -39,26 +39,23 @@ int cmd_threaten(string str)
         return 1;
     }
 
-    switch(LEVELCHECK->levelcheck(TP,targ))
-    {
-    case 0:
+    if(TP->query_death_flag() || TP->get_pk_death_flag() || TP->query("no pk")) {
+        tell_object(TP,"%^YELLOW%^You can't engage in PK roleplay with a Death, Grace Period or NoPK flag of your own!%^RESET%^");
+        return 1;
+    }
+
+    if(targ->query_death_flag() || targ->get_pk_death_flag() || targ->query("no pk")) {
         player = (string)targ->query_name();
         tell_object(TP,"%^BOLD%^%^CYAN%^You gather your most impressive and frightening look about you, hinting them that they are pushing their luck.\n%^YELLOW%^Give the player the benefit of a moment or two to RP an appropriate reaction. If they continue to provoke your character, you are within rights to instigate a PK situation with them.%^RESET%^");
 
-        tell_object(targ,"%^BOLD%^%^CYAN%^"+TP->query_name()+" takes on a very threatening air towards you.\n%^YELLOW%^You get the impression that you are pushing your luck in provoking this character (see %^WHITE%^<help threaten>%^YELLOW%^). If you wish to continue to do so, you must be prepared to enter a pkill situation. Otherwise you will need to roleplay appropriate respect or fear of the player, or find an appropriate way to extract yourself from the situation.%^RESET%^");
+        tell_object(targ,"%^BOLD%^%^CYAN%^"+TP->QCN+" %^BOLD%^%^CYAN%^takes on a very threatening air towards you.\n%^YELLOW%^You get the impression that you are pushing your luck in provoking this character (see %^WHITE%^<help threaten>%^YELLOW%^). If you wish to continue to do so, you must be prepared to enter a pkill situation. Otherwise you will need to roleplay appropriate respect or fear of the player, or find an appropriate way to extract yourself from the situation.%^RESET%^");
         seteuid(UID_LOG);
         log_file("player/threaten","%^BOLD%^%^CYAN%^"+ctime(time())+"%^RESET%^: Threaten "
                  "command used by "+TP->query_name()+" against "+player+".\n");
         seteuid(getuid());
         return 1;
-    case 1:
-        tell_object(TP,"%^BOLD%^%^GREEN%^Just kill them, you're allowed.%^RESET%^");
-        return 1;
-    default:
-        tell_object(TP,"%^BOLD%^%^RED%^You can't engage in PK with your target due to flags set.%^RESET%^");
-        return 1;
-        break;
     }
+    tell_object(TP,"%^BOLD%^%^GREEN%^There is nothing preventing you from engaging in PK roleplay with this player.%^RESET%^");
     return 1;
 }
 
@@ -67,7 +64,7 @@ void help()
     write("
 %^CYAN%^NAME%^RESET%^
 
-threaten - warn a lower player that they are provoking you to PK
+threaten - warn a player that they are provoking you to PK
 
 %^CYAN%^SYNOPSIS%^RESET%^
 
@@ -75,11 +72,9 @@ threaten %^ULINE%^%^ORANGE%^PLAYER%^RESET%^
 
 %^CYAN%^DESCRIPTION%^RESET%^
 
-The command will run levelcheck to identify validity of the threat first, then if you're threatening lower player they will recieve a warning that their next action might result in Player Kill.
+The command will determine if you are currently able to engage in PK roleplay; then the targeted %^ULINE%^%^ORANGE%^PLAYER%^RESET%^ under PK protection will recieve a warning that their next action might result in Player Kill.
 
-If %^ULINE%^%^ORANGE%^PLAYER%^RESET%^ continues to provoke you, you're free to engage in Player Kill.
-
-Failure to warn a much lower level character may result in retcon and punishing, depending on the current Player Kill policy.
+If %^ULINE%^%^ORANGE%^PLAYER%^RESET%^ continues to provoke you, you're free to engage in Player Kill and/or bring the situation to the attention of an immortal.
 
 %^CYAN%^SEE ALSO%^RESET%^
 
