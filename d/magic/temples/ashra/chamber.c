d651fa9eeeb2095553291b46708f25afd715e33b
shadowgate <law@shadowgate.org>
2021-06-22 01:00:05 +0000

    Autocommit

--- d/magic/temples/ashra/chamber.c
+++ d/magic/temples/ashra/chamber.c
@@ -38,6 +38,25 @@ void create()
     ]));
 }
 
+void reset()
+{
+    object ob;
+    ::reset();
+    
+    if(!present("mirror"))
+    {
+        ob = new("/d/magic/obj/mirror");
+        
+        if(ob)
+        {
+            ob->set_short("%^BOLD%^BLACK%^A glossy black table with a mirror finish%^RESET%^");
+            ob->set_long("%^BOLD%^%^BLACK%^This luxurious round table rises from a central pillar that is anchored to the floor somewhere beneath the murky darkness underfoot. A %^RESET%^%^MAGENTA%^ring %^BOLD%^%^BLACK%^of %^RESET%^%^MAGENTA%^deep purple %^BOLD%^%^BLACK%^pine surrounds the glossy obs%^WHITE%^i%^BLACK%^d%^RESET%^i%^BOLD%^a%^BLACK%^n disk set in its center. The surface of the obsidian is absolutely flawless and refl%^RESET%^e%^BOLD%^%^BLACK%^ct%^WHITE%^i%^BLACK%^ve like a pitch black %^RESET%^mirror%^BOLD%^%^BLACK%^. Surrounding the table are a collection of %^RESET%^%^MAGENTA%^matching chairs%^BOLD%^%^BLACK%^, %^RESET%^%^MAGENTA%^dark purple pine %^BOLD%^%^BLACK%^with glossy black cushions.%^RESET%^");
+            ob->set_id( ({ "mirror", "fine mirror", "table", "black table", "mirror table" }) );
+            ob->move(this_object());
+        }
+    }
+}
+
 /*
 Items: Obsidian round table    Descrip: %^BOLD%^%^BLACK%^This luxurious round table rises from a central pillar that is anchored to the floor somewhere beneath the murky darkness underfoot. A %^RESET%^%^MAGENTA%^ring %^BOLD%^%^BLACK%^of %^RESET%^%^MAGENTA%^deep purple %^BOLD%^%^BLACK%^pine surrounds the glossy obs%^WHITE%^i%^BLACK%^d%^RESET%^i%^BOLD%^a%^BLACK%^n disk set in its center. The surface of the obsidian is absolutely flawless and refl%^RESET%^e%^BOLD%^%^BLACK%^ct%^WHITE%^i%^BLACK%^ve like a pitch black %^RESET%^mirror%^BOLD%^%^BLACK%^. Surrounding the table are a collection of %^RESET%^%^MAGENTA%^matching chairs%^BOLD%^%^BLACK%^, %^RESET%^%^MAGENTA%^dark purple pine %^BOLD%^%^BLACK%^with glossy black cushions.%^RESET%^
                                              *Acts as mirror for scrying*
