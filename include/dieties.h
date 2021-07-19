b8f3b23774e2b7d5af36ad58e01275482d78204e
shadowgate <law@shadowgate.org>
2021-07-19 17:00:04 +0000

    Autocommit

--- include/dieties.h
+++ include/dieties.h
@@ -9,14 +9,14 @@
    "callamir":({"Luck/Trickery",({1, 4, 5, 7, 8, 9}),({4, 7, 8})}),\
    "cevahir":({"War/Strategy",({1, 2, 3, 4, 5, 6, 7, 8, 9}),({2, 4, 5, 6, 8})}),\
    "jarmila":({"Light/Love",({1, 2, 4, 5, 6, 7, 8}),({1, 4, 5, 7})}),    \
-   "khyron":({"Storms/Destruction",({2, 4, 5, 6, 7, 8, 9}),({5, 7, 8, 9})}), \
+   "khyron":({"Storms/Destruction",({3, 5, 6, 7, 8, 9}),({6, 8, 9})}), \
    "kismet":({"Magic/Knowledge",({1, 2, 3, 4, 5, 6, 8, 9}),({2, 4, 5, 6, 8})}), \
    "kreysneothosies":({"Justice/Protection",({1, 2, 3, 4, 5, 7}),({1, 2, 4})}), \
    "lord shadow":({"Darkness/Tyranny",({1, 2, 3, 5, 6, 9}),({2, 3, 6})}),\
    "lysara":({"Death/Fate",({1, 2, 3, 4, 5, 6, 7, 8, 9}),({2, 4, 5, 6, 8})}),\
    "nilith":({"Undead/War",({3, 5, 6, 7, 8, 9}),({6, 8, 9})}),\
-   "nimnavanon":({"Forests/Weather",({1, 2, 3, 4, 5, 6, 7, 8, 9}),({2, 4, 5, 6, 8})}), \
-   "seija":({"Magic/Knowledge",({1, 2, 3, 4, 5, 6, 8}),({1, 2, 3, 5})}),\
+   "nimnavanon":({"Forests/Weather",({1, 2, 4, 5, 6, 7, 8}),({1, 4, 5, 7})}), \
+   "seija":({"Magic/Knowledge",({1, 2, 3, 4, 5, 6, 7, 8, 9}),({2, 4, 5, 6, 8})}),\
    "the faceless one":({"Murder/Deception",({2, 3, 4, 5, 6, 8, 9}),({3, 5, 6, 9})}),\
 ]))
 
