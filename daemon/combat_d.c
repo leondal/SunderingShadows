54b950f066d4d2b4b339d204ca58a57b49c49323
shadowgate <law@shadowgate.org>
2021-07-24 22:00:05 +0000

    Autocommit

--- daemon/combat_d.c
+++ daemon/combat_d.c
@@ -87,6 +87,9 @@ varargs int extra_hit_calcs(object attacker, object victim, object weapon, strin
         else
             MissChance -= 5;
     }
+
+    if(FEATS_D->usable_feat(victim, "inconstant position"))
+        MissChance += 10;
     
     if (mount && FEATS_D->usable_feat(rider, "mounted shield")) {
         ShieldMissChance = (int)rider->query_shieldMiss();
