a89de1f4b3c84322f9ef3ed52535e39c8b531bea
shadowgate <law@shadowgate.org>
2021-08-01 22:00:06 +0000

    Autocommit

--- cmds/feats/r/_rage.c
+++ cmds/feats/r/_rage.c
@@ -287,7 +287,7 @@ void execute_attack()
     if(FEATS_D->usable_feat(caster, "animal fury"))
     {
         target = caster->query_current_attacker();
-        enchant = COMBAT_D->unarmed_enchantment(caster);
+        enchant = COMBAT_D->unarmed_enchantment(caster) + BONUS_D->query_stat_bonus(caster, "strength");
         
         if(target && thaco(target, enchant))
         {
@@ -333,7 +333,7 @@ void execute_attack()
             tell_object(caster, "%^YELLOW%^You rake your claws across " + target->QCN + "'s chest!");
             tell_object(target, caster->QCN + " rakes " + caster->QP + " claws across your chest!");
             tell_room(place, caster->QCN + " rakes " + caster->QP + " claws across " + target->QCN + "'s chest!");
-            dam = roll_dice(2, 6) + enchant + BONUS_D->query_stat_bonus(caster, "strength");
+            dam = (roll_dice(1, 6) * (1 + flevel / 10)) + enchant;
             caster->cause_typed_damage(target, target->return_target_limb(), dam, "slashing");
         }
     }   
