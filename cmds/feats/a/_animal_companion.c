ce5abae16bb3cc09672f890ba6f7b04ffdc9101c
Yves <mana.starved@yahoo.com>
2021-08-14 11:33:24 -0700

    Add feat classes and use feat level for calculations

--- cmds/feats/a/_animal_companion.c
+++ cmds/feats/a/_animal_companion.c
@@ -1,9 +1,9 @@
 /*
   _animal_companion.c
-  
+
   Feat that allows a ranger to summon an animal
   companion.
-  
+
   -- Tlaloc -- 2.18.20
 */
 
@@ -16,7 +16,7 @@
 inherit FEAT;
 
 //Format - type : ({ STR, DEX, CON, INT, WIS, CHA, SIZE, AC BONUS, }),
-mapping valid_types = ([ 
+mapping valid_types = ([
                          "ape" :       ({ 13, 17, 10, 2, 12, 7, 3, 2,  }),
                          "badger" :    ({ 10, 17, 15, 2, 12, 10, 2, 4, }),
                          "bear" :      ({ 15, 15, 13, 2, 12, 6, 3, 4,  }),
@@ -42,6 +42,7 @@ void create()
     feat_category("SavageCombat");
     feat_name("animal companion");
     feat_prereq("Druid L1 or Ranger L4");
+    feat_classes(({"druid", "ranger"}));
     feat_syntax("animal_companion [TYPE] to summon or animal_companion to dismiss");
     feat_desc("Summons or dismisses your faithful animal companion. This companion will follow you through thick and thin, will level up with you, and gains several perks as it grows. The TYPE of animal will determine its base stats and natural armor.  The animal companion gains its own feats as follows:
 
@@ -49,7 +50,7 @@ void create()
   LEVEL  6 - Resistance
   LEVEL  9 - Precise Strikes
   LEVEL 15 - Stalwart
-  
+
 The Animal Companion will hide if you use the 'hide_in_shadows' command, allowing you to effectively sneak about with your faithful friend.
 
 The Animal Companion can also be customized through several commands, which will allow you to change its description:
@@ -57,9 +58,9 @@ The Animal Companion can also be customized through several commands, which will
   'animal short [DESCRIPTION]' - Changes the short description of the companion.
   'animal long  [DESCRIPTION]' - Changes the long description of the companion.
 
-  To have the animal companion follow you, use 'animal follow'.  
+  To have the animal companion follow you, use 'animal follow'.
   To command the animal companion, use %^ORANGE%^<animal command %^ULINE%^ACTION%^RESET%^ORANGE%^>.%^RESET%^");
-  
+
     set_target_required(0);
 }
 
@@ -69,39 +70,39 @@ int prerequisites(object ob)
 {
     if(!objectp(ob))
         return 0;
-    
+
     if(ob->query_class_level("ranger") < 4 && ob->query_class_level("druid") < 1)
     {
         dest_effect();
         return 0;
     }
-   
+
     return ::prerequisites(ob);
 }
 
 int cmd_animal_companion(string str)
 {
     object feat;
-    
+
     if(!objectp(this_player()))
         return 0;
-    
+
     feat = new(base_name(this_object()));
     feat->setup_feat(this_player(), str);
-    
+
     return 1;
 }
 
 void execute_feat()
 {
     object ob;
-    
+
     int class_level,
         comp_hd,
         comp_ac;
-    
+
     ::execute_feat();
-    
+
     if(caster->query_property("using instant feat"))
     {
         tell_object(caster,"You are already in the middle of using a feat!");
@@ -109,24 +110,24 @@ void execute_feat()
         return;
     }
 
-    
+
     if(sizeof(caster->query_attackers()))
     {
         tell_object(caster, "You can't summon your animal companion during combat!");
         return;
     }
-    
-    
+
+
     companion = caster->query_property("animal_companion");
     pack = caster->query_protectors();
     pack = filter_array(pack, (: $1->query_pack_member() :));
-    
+
     if(objectp(companion) || sizeof(pack))
     {
         companion && tell_object(caster, "You dismiss your animal companion.");
         caster->remove_property("animal_companion");
         companion && companion->remove();
-        
+
         if(sizeof(pack))
         {
             foreach(object obj in pack)
@@ -138,29 +139,30 @@ void execute_feat()
                 if(objectp(obj))
                     destruct(obj);
             }
-            
+
             pack = ({  });
         }
-                
+
         return;
     }
-    
+
     pack = ({  });
-    
+
     if(!arg || member_array(arg, keys(valid_types)) < 0)
     {
         tell_object(caster, "That is not a valid choice for your animal companion.");
         tell_object(caster, "Valid options are: (" + implode(m_indices(valid_types), ",") + ") ");
         return;
     }
-    
+
     tell_object(caster, sprintf("You summon your trusty %s companion%s to your side.", arg, caster->query_chosen_animal() == arg ? "s" : ""));
-    
-    class_level = caster->query_prestige_level("ranger") + caster->query_prestige_level("druid");
-    
+
+    //class_level = caster->query_prestige_level("ranger") + caster->query_prestige_level("druid");
+    class_level = flevel;
+
     comp_hd = class_level + 2;
     comp_ac = class_level + 10;
-    
+
     companion = new("/d/magic/mon/acompanion");
     companion->set_race(arg);
     companion->set_name(arg);
@@ -174,7 +176,7 @@ void execute_feat()
     companion->set_hp(14 * comp_hd + 14);
     companion->set_alignment(caster->query_alignment());
     companion->set_owner(caster);
-       
+
     caster->set_property("animal_companion", companion);
     caster->add_follower(companion);
     caster->add_protector(companion);
@@ -182,7 +184,7 @@ void execute_feat()
     companion->set_property("minion", caster);
     companion->move(environment(caster));
     companion->set_heart_beat(1);
-    
+
     //Setting companion stats based on type per SRD
     companion->set_stats("strength", valid_types[arg][0] + min( ({ class_level / 5, 6 }) ) );
     companion->set_stats("dexterity", valid_types[arg][1] + min( ({ class_level / 5, 6 }) ) );
@@ -192,7 +194,7 @@ void execute_feat()
     companion->set_stats("charisma", valid_types[arg][5]);
     companion->set_size(valid_types[arg][6]);
     companion->set_overall_ac(0 - comp_ac - valid_types[arg][7]);
-  
+
     //Based on SRD - companion gets "specials" at certain caster levels
     if(class_level >= 3)
         companion->set_monster_feats( ({ "evasion" }) );
@@ -202,7 +204,7 @@ void execute_feat()
         companion->set_monster_feats( ({ "evasion", "resistance", "precise strikes" }) );
     if(class_level >= 15)
         companion->set_monster_feats( ({ "evasion", "resistance", "precise strikes", "stalwart" }) );
-    
+
     if(caster->query_chosen_animal() == arg)
     {
         //Chosen animal greatly buffs the animal companion
@@ -211,7 +213,7 @@ void execute_feat()
             companion->set_property("damage resistance", 10);
             companion->set_monster_feats( ({ "toughness", "improved toughness", "evasion", "resistance", "precise strikes", "stalwart", "rapid strikes" }) );
         }
-    
+
         if(FEATS_D->usable_feat(caster, "pack leader"))
         {
             for(int x = 0; x < BEAST_MOD; x++)
@@ -231,14 +233,14 @@ void execute_feat()
                 pack_animal->set_alignment(caster->query_alignment());
                 pack_animal->set_owner(caster);
                 pack_animal->set_property("damage resistance", 10);
-       
+
                 caster->add_follower(pack_animal);
                 caster->add_protector(pack_animal);
 
                 pack_animal->set_property("minion", caster);
                 pack_animal->move(environment(caster));
                 pack_animal->set_heart_beat(1);
-    
+
                 //Setting companion stats based on type per SRD
                 pack_animal->set_stats("strength", valid_types[arg][0] + min( ({ class_level / 5, 6 }) ) );
                 pack_animal->set_stats("dexterity", valid_types[arg][1] + min( ({ class_level / 5, 6 }) ) );
@@ -248,7 +250,7 @@ void execute_feat()
                 pack_animal->set_stats("charisma", valid_types[arg][5]);
                 pack_animal->set_size(valid_types[arg][6]);
                 pack_animal->set_overall_ac(0 - comp_ac - valid_types[arg][7]);
-                
+
                 //Based on SRD - companion gets "specials" at certain caster levels
                 if(class_level >= 3)
                     pack_animal->set_monster_feats( ({ "evasion" }) );
@@ -261,7 +263,7 @@ void execute_feat()
             }
         }
     }
-       
+
     return;
 }
 
