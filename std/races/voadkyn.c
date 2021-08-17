91cf42bbe57cb253b9ce04b869b37da95a1e71b5
shadowgate <law@shadowgate.org>
2021-08-17 15:30:04 +0000

    Autocommit

--- std/races/voadkyn.c
+++ std/races/voadkyn.c
@@ -18,12 +18,12 @@ string *restricted_deities(string subrace) {
 }
 
 // stats in order: str, dex, con, int, wis, cha
-int *stat_mods(string subrace) { return ({ 2, 4, -2, 0, -2, 0 }); }
+int *stat_mods(string subrace) { return ({ 4, -2, 2, -2, 0, 0 }); }
 
 mapping skill_mods(string subrace) { return ([ "stealth" : 2, "survival" : 2  ]); }
 
 
-int natural_AC(string subrace) { return 2; }
+int natural_AC(string subrace) { return 4; }
 
 int sight_bonus(string subrace) { return 2; }
 
