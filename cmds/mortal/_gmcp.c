11601b7466b2560ebdf19ede0bae66044ac607ad
shadowgate <law@shadowgate.org>
2021-09-17 00:00:04 +0000

    Autocommit

--- cmds/mortal/_gmcp.c
+++ cmds/mortal/_gmcp.c
@@ -1,10 +1,14 @@
 /*
   _gmcp.c
   
-  Player command to enable/disable or check status of GMCP.
+  Player command to send send gmcp info from game.
   
   -- Tlaloc -- 9.16.21
 */
+
+#include <std.h>
+#include <daemons.h>
+#include <new_exp_table.h>
 
 int cmd_gmcp(string str)
 {
@@ -16,14 +20,29 @@ int cmd_gmcp(string str)
     if(gmcp_enabled)
     {
         mapping char_info = ([
-                              "hp"    : this_player()->query_hp() + "",
-                              "maxhp" : this_player()->query_max_hp() + "",
+                              "hp"        : this_player()->query_hp() + "",
+                              "maxhp"     : this_player()->query_max_hp() + "",
+                              "encumb"    : this_player()->query_internal_encumbrance() + "",
+                              "maxencumb" : this_player()->query_max_internal_encumbrance() + "",
+                              "stamina"   : this_player()->query_condition_percent() + "",
+                              "pp"        : this_player()->query_mp() + "",
+                              "maxpp"     : this_player()->query_max_mp() + "",
+                              "xp"        : english_number(this_player()->query_exp()) + "",
+                              "grace"     : this_player()->query("available grace") + "",
+                              "maxgrace"  : this_player()->query("max grace") + "",
+                              "arcana"    : this_player()->query("available arcana") + "",
+                              "maxarcana" : this_player()->query("max arcana") + "",
+                              
                             ]);
                             
         res = "Char.Vitals " + json_encode(char_info) + "\n";
         
         printf("<GMCP sending : %s\n", res);
         this_player()->send_gmcp(res);
+    }
+    else
+    {
+        write("<GMCP not enabled.>\n");
     }
     
     return 1;
