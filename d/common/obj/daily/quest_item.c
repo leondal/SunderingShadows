a1e226be3f64c17890e9a1454bb017f3197cd963
shadowgate <law@shadowgate.org>
2021-09-20 17:00:05 +0000

    Autocommit

---
+++ d/common/obj/daily/quest_item.c
@@ -0,0 +1,50 @@
+/*
+  quest_item.c
+  
+  Similar to the alt reality quest item, this is what the player
+  is seeking out when they enter the daily demiplane.
+  
+  -- Tlaloc --
+*/
+
+#include <std.h>
+#include <dameons.h>
+
+inherit OBJECT;
+
+void create()
+{
+    ::create();
+    
+    set_id(({"alt world quest item", "fragment", "fragment of reality", "reality fragment"}));
+    set_name("fragment of reality");
+    set_short(CRAYON_D->color_string("Fragment of Reality", "dark red"));   
+    
+    set_long("%^BOLD%^%^RED%^This item continually glows and gives of an aura that makes you nauseous simply "+
+    "looking at it. You are not exactly sure what it is, but you realize that it is powerful, almost beyond "+
+    "comprehension. You think that it is somehow connected to the world that you are in, as if it's existence "+
+    "parallels that of the world around you. Actually, as crazy as it seems, it is almost like this item "+
+    "controls the existence of the world around you. You think that there is a tremendously perverse magic at play here "+
+    "and shudder visibly as you look over the item. You have an overwhelming urge to touch it, to pick it up, to let "+
+    "your warmth encompass it. Though you have no idea why or from where this urge comes from, it seems extremely "+
+    "important to you.%^RESET%^");      
+    
+    set_property("no animate", 1);
+    set_weight(0);
+}
+
+void init()
+{
+    object holder;
+    
+    ::init();
+    
+    holder = environment(this_object());
+    
+    if(!holder || !userp(holder))
+        return;
+}
+
+
+    
+    
